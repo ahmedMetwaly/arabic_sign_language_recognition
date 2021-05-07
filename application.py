c9d3529f4e4ce3ecfdf27f9f5918986f56b09a0b
Ahmed Mohamed <62860063+ahmedMetwaly@users.noreply.github.com>
2021-05-07 20:35:52 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,30 +1,10 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-from sklearn.metrics import accuracy_score # Accuracy metrics 
-import pickle
-from sklearn.pipeline import make_pipeline 
-from sklearn.preprocessing import StandardScaler 
-from sklearn.linear_model import LogisticRegression, RidgeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-import pandas as pd
-import csv
-import os
-from matplotlib import pyplot as plt
-import uuid
-import time
-import cv2
-import mediapipe as mp # Import mediapipe
+
 
 
 app = Flask(__name__)
 with open('signs4.pkl', 'rb') as f:
     model = pickle.load(f)
 
-def isInitialized(hand):
-    try:
-        if hand.IsInitialized() == True:
-            return True
-    except: return False
 
 @app.route('/')
 @app.home('/home')
